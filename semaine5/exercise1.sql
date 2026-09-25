d5b513710418a8680f0adc7257de28bede7d85d9
Andres Marquez <andresmr955@gmail.com>
2026-09-25 08:14:28 -0400

    finishing ex 3.3

--- semaine5/exercise1.sql
+++ semaine5/exercise1.sql
@@ -23,3 +23,45 @@ VALUES
     (6, 'Annie Leroy', 'F', 'Multimedia', 90.0, 87.5),
     (7, 'Valdislav Koval', 'M', 'Informatique', 84.5, 87.0),
     (8, 'Niko Petra', 'F', 'Informatique', 73.0, 71.0);
+
+-- 1- Sélectionner tous les élèves et afficher leur nom et leur NoteFinale=
+-- (noteExamen1+ noteExamen2)/2, avec la NoteFinale arrondie à l'entier le plus proche.
+SELECT * FROM eleves_exercise1;
+
+SELECT nom, CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2 )AS DECIMAL(5, 2)) AS NoteFinal
+FROM eleves_exercise1
+
+-- 2- Sélectionner tous les élèves et afficher leur nom en majuscules accompagné 
+-- de leur NoteFinale .
+
+SELECT UPPER(nom) as Nom,CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2) AS DECIMAL(5, 2)) AS NoteFinal
+FROM eleves_exercise1;
+
+SELECT LOWER(LEFT(nom, 1)) + SUBSTRING(nom, 2, LEN(nom)), CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2)AS DECIMAL(5,2)) AS NoteFinal
+FROM eleves_exercise1;
+
+-- 3- Afficher toutes les informations de l'élève ayant obtenu la note finale 
+-- la plus élevée.
+
+SELECT TOP 1 *, 
+CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2) AS DECIMAL(5, 2)) AS NoteFinal
+FROM eleves_exercise1
+ORDER BY NoteFinal DESC;
+
+SELECT *,
+CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2) AS DECIMAL (5,2)) AS NoteFinal
+FROM eleves_exercise1
+WHERE CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2) AS DECIMAL (5,2)) =
+(
+SELECT MAX((noteExamen1 + noteExamen2) / 2)
+FROM eleves_exercise1
+)
+
+DECLARE @NoteMax DECIMAL(5,2);
+SET @NoteMax = (SELECT MAX(CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2) AS DECIMAL(5,2))) FROM eleves_exercise1)
+SELECT @NoteMax
+
+SELECT *,
+CAST(ROUND((noteExamen1 + noteExamen2) / 2, 2) AS DECIMAL(5,2)) AS NoteFinal
+FROM eleves_exercise1
+WHERE CAST(ROUND((noteExamen)) DECIMAL (5,2))
